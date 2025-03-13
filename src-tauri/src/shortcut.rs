549fc9cfa68770d32414fc3649d3c1d602bc7d5b
initialencounter <2911583893@qq.com>
2025-03-14 00:06:09 +0800

    feat: update dependencies and refactor shortcut handling for better platform support

--- src-tauri/src/shortcut.rs
+++ src-tauri/src/shortcut.rs
@@ -1,112 +1,51 @@
+use tauri::{Emitter, WebviewWindow};
+
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "macos")]
+use rdev::{grab, Event, EventType, Key};
+
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "macos")]
 use std::sync::{Arc, Mutex};
 
-use rdev::{Event, EventType, grab, Key};
-use tauri::{Emitter, WebviewWindow};
-// use std::ptr::null_mut;
-// use std::sync::{ Once, OnceLock };
-// use windows::Win32::Foundation::{HINSTANCE, LPARAM, LRESULT, WPARAM};
-// use windows::Win32::UI::WindowsAndMessaging::{CallNextHookEx, HHOOK, KBDLLHOOKSTRUCT, MSG,
-//                                               SetWindowsHookExW, UnhookWindowsHookEx,
-//                                               WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP};
-// use windows::Win32::UI::WindowsAndMessaging;
+#[cfg(target_os = "windows")]
+use windows::Win32::UI::{
+    Input::KeyboardAndMouse::{RegisterHotKey, HOT_KEY_MODIFIERS},
+    WindowsAndMessaging::{GetMessageW, MSG, WM_HOTKEY},
+};
 
-// static mut MODIFIER_STATE: Option<ModifierState> = None;
-// static INIT: Once = Once::new();
-// 
-// struct ModifierState {
-//     ctrl: bool,
-//     shift: bool,
-// }
-// 
-// #[derive(Debug)]
-// struct HookHandler {
-//     window: WebviewWindow,
-// }
-// 
-// impl HookHandler {
-//     fn handle_keypress(&self) {
-//         // 使用外部参数
-//         self.window.emit("KeyPressed", Some(())).unwrap();
-//     }
-// }
-// 
-// // 静态全局变量保存 HookHandler 实例
-// static HOOK_HANDLER: OnceLock<HookHandler> = OnceLock::new();
-// 
-// unsafe extern "system" fn keyboard_proc(ncode: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-//     if ncode == 0 {
-//         let kb_struct = *(lparam.0 as *const KBDLLHOOKSTRUCT);
-//         let vk_code = kb_struct.vkCode;
-// 
-//         INIT.call_once(|| {
-//             MODIFIER_STATE = Some(ModifierState { ctrl: false, shift: false });
-//         });
-// 
-//         let modifier_state: &mut ModifierState = MODIFIER_STATE.as_mut().unwrap();
-// 
-//         // 处理按键按下事件
-//         if wparam.0 == WM_KEYDOWN as usize {
-//             match vk_code {
-//                 162 => modifier_state.ctrl = true,
-//                 160 => modifier_state.shift = true,
-//                 67 => {
-//                     if modifier_state.ctrl && modifier_state.shift {
-//                         if let Some(handler) = HOOK_HANDLER.get() {
-//                             handler.handle_keypress();
-//                             return LRESULT(1);
-//                         }
-//                     }
-//                 }
-//                 _ => {
-//                     ()
-//                 }
-//             }
-//         }
-//         // 处理按键释放事件
-//         else if wparam.0 == WM_KEYUP as usize {
-//             match vk_code {
-//                 162 => modifier_state.ctrl = false,
-//                 160 => modifier_state.shift = false,
-//                 _ => {
-//                     ()
-//                 }
-//             }
-//         }
-//     }
-// 
-//     CallNextHookEx(None, ncode, wparam, lparam)
-// }
-// 
-// 
-// pub fn win32shortcut(window: WebviewWindow) {
-//     // 初始化全局的 HookHandler
-// 
-//     HOOK_HANDLER.set(HookHandler { window }).unwrap();
-// 
-//     unsafe {
-//         // 设置钩子并处理可能的错误
-//         let hook_result = SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_proc), HINSTANCE(null_mut()), 0);
-// 
-//         // 确保钩子成功设置
-//         let hook_id: HHOOK = match hook_result {
-//             Ok(hook) => hook,
-//             Err(err) => {
-//                 eprintln!("Failed to set hook: {:?}", err);
-//                 return;
-//             }
-//         };
-// 
-//         let mut msg: MSG = MSG::default();
-//         while WindowsAndMessaging::GetMessageW(&mut msg, None, 0, 0).into() {
-//             let _ = WindowsAndMessaging::TranslateMessage(&msg);
-//             WindowsAndMessaging::DispatchMessageW(&msg);
-//         }
-// 
-//         // 卸载钩子
-//         UnhookWindowsHookEx(hook_id).unwrap();
-//     }
-// }
+#[cfg(target_os = "windows")]
+async fn listen_hotkey<F, Fut>(mut callback: F)
+where
+    F: FnMut() -> Fut,
+    Fut: futures::Future<Output = bool>,
+{
+    unsafe {
+        let _ = RegisterHotKey(None, 1, HOT_KEY_MODIFIERS(0x0006), 0x43);
+        let mut msg = MSG::default();
+        while GetMessageW(&mut msg, None, 0, 0).as_bool() {
+            if msg.message == WM_HOTKEY && !callback().await {
+                break;
+            }
+        }
+    }
+}
+#[cfg(target_os = "windows")]
+pub fn rdev_shortcut(window: WebviewWindow) {
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async {
+            listen_hotkey(move || {
+                window.emit("KeyPressed", Some(())).unwrap();
+                async move { true }
+            })
+            .await;
+        });
+    });
+}
 
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "macos")]
 pub fn rdev_shortcut(window: WebviewWindow) {
     tauri::async_runtime::spawn(async move {
         let is_ctrl_press = Arc::new(Mutex::new(false));
@@ -125,9 +64,7 @@ pub fn rdev_shortcut(window: WebviewWindow) {
                         return None;
                     }
                 }
-                _ => {
-                    ()
-                }
+                _ => (),
             }
             Some(event)
         };
